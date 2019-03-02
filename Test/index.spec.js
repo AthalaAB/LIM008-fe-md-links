ccde1558498fe5f116aa97c0445a39fe5c50a035
AthalaAB <athala.abanto@gmail.com>
2019-03-01 20:42:05 -0500

    Implementación de funciones: si es directorio, si en archivo, y sus test, instalando Babel con error

--- Test/index.spec.js
+++ Test/index.spec.js
@@ -1,13 +1,56 @@
-// import { pathToBeAbsolute, relativeToAbsolute } from "../src/module/path";
+import { pathToBeAbsolute, relativeToAbsolute, pathIsDirectory, pathIsFile } from "../src/module/path";
 
-// describe('pathToBeAbsolute', () => {
-//     it('deberia ser una funcion', () => {
-//         expect(typeof pathToBeAbsolute).toBe('function');
-//     });
-// });
 
-// describe('relativeToAbsolute', () => {
-//     it('deberia ser una funcion', () => {
-//         expect(typeof relativeToAbsolute).toBe('function');
-//     });
-// });
+describe('pathToBeAbsolute', () => {
+    it('deberia ser una funcion', () => {
+        expect(typeof pathToBeAbsolute).toBe('function');
+    });
+});
+
+describe('pathToBeAbsolute', () => {
+    it('deberia retorar true', () => {
+        expect(pathToBeAbsolute('/test/demo_path.js')).toBe(true);
+    });
+});
+
+describe('pathToBeAbsolute', () => {
+    it('deberia retornar false', () => {
+        expect(pathToBeAbsolute('test/demo_path.js')).toBe(false);
+    });
+});
+
+describe('relativeToAbsolute', () => {
+    it('deberia ser una funcion', () => {
+        expect(typeof relativeToAbsolute).toBe('function');
+    });
+});
+
+describe('relativeToAbsolute', () => {
+    it('deberia retornar ruta absoluta', () => {
+        expect(relativeToAbsolute('test/demo_path.js')).toBe('C:\\Users\\Laboratoria\\Desktop\\MD-LINKS\\LIM008-fe-md-links\\test\\demo_path.js');
+    });
+});
+
+describe('pathIsDirectory', () => {
+    it('deberia ser una funcion', () => {
+        expect(typeof pathIsDirectory).toBe('function');
+    });
+});
+
+describe('pathIsDirectory', () => {
+    it ('deberia retornar true', () => {
+    expect(pathIsDirectory('.\\test')).toBe(true);
+    });
+});
+
+describe('pathIsFile', () => {
+    it('deberia ser una funcion', () => {
+        expect(typeof pathIsFile).toBe('function');
+    });
+});
+
+describe('pathIsFile', () => {
+    it ('deberia retornar true', () => {
+    expect(pathIsFile('Test\\prueba.js')).toEqual(true);
+    });
+});
--- src/module/path.js
+++ src/module/path.js
@@ -1,9 +1,19 @@
 const path = require('path');
+const fs = require('fs');
+let result = [];
 
-export const pathToBeAbsolute = (pathEvaluate) => {
-    return path.isAbsolute(pathEvaluate) 
+export const pathToBeAbsolute = (pathToEvaluate) => {
+    return path.isAbsolute(pathToEvaluate) 
 }
 
 export const relativeToAbsolute = (pathRelative) => {
     return path.resolve(pathRelative)
 }
+
+export const pathIsDirectory = (pathToEvaluate) => {
+    return fs.lstatSync(pathToEvaluate).isDirectory();
+ }
+
+ export const pathIsFile = (pathToEvaluate) => {
+    return fs.lstatSync(pathToEvaluate).isFile();
+ };
